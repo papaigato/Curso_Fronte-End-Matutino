183ba5100ac6761652d9f329e9704555d7504337
gustavo <gvsgugu08@gmail.com>
2023-08-31 11:41:09 -0300

    git commit

---
+++ html/day12-atvJS/atv-java/script.js
@@ -0,0 +1,44 @@
+// alert('Hellow word! com js')
+
+// prompt('informe seu nome:')
+
+//confirm('tem certeza, que vc dejese vender sua alma pra mim:')
+
+//console.log('usando o console')
+
+//if(true){
+//        var numero02;
+//        numero02=10
+//        console.log(numero02)
+//}
+//console.log(numero02)
+
+
+//var numero01;
+//let numero02;
+//const numero03 = 10;
+
+//var nome;
+
+//nome='gustavo'
+
+//console.log(typeof(nome))
+
+//var n1 = 'lili'    
+//var n2 = null
+
+//var soma = n1 - n2
+//var soma = null
+//var caneca = {
+//        nome:'teu cu',
+//        cor: 'colorida',
+//        educativa:true
+//}
+
+//var exVetor=['caneca', 'cafe']
+
+
+
+//console.log(exVetor)
+//console.log(typeof(exVetor))
+

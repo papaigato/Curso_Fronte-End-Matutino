8ceef4ea5894ac67313ff909a5e561aa46f458cb
gustavo <gvsgugu08@gmail.com>
2023-09-01 11:38:46 -0300

    git commit

---
+++ html/day13/script.js
@@ -0,0 +1,18 @@
+const titulo=window.document.getElementsByTagName('h1')
+
+const divis = window.document.getElementsByClassName('quadrado')
+const redonda = window.document.getElementsByClassName('circulo')
+
+// querySeletor
+// querySeletorALL
+
+//const lista = document.querySelectorAll('.quadrado')
+const lista = document.querySelectorAll('ul')
+
+console.log()
+
+// console.log(titulo)
+// console.log(divis)
+// console.log(redonda)
+//console.log(titulo)
+console.log(lista)

b1025e9d48bf91d4ab0355cd47bebc627ecde6d1
gustavo <gvsgugu08@gmail.com>
2023-09-12 11:36:13 -0300

    correçao atv17

---
+++ javascript/aula04-atv/atv17/script.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menusimbulo");
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("hide-icon");
+}); 
